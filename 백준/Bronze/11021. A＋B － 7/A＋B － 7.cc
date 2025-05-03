d0d34956e37c609e47031e1568470336a4eb0f5e
xode114kr1 <144416796+xode114kr1@users.noreply.github.com>
2025-05-03 21:31:16 +0900

    [Bronze V] Title: A+B - 7, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.cc
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main()
+{
+  int i,rep,a,b;
+  scanf("%d",&rep);
+  for(i = 1; i<=rep;i++)
+    {
+      scanf("%d %d",&a,&b);
+      printf("Case #%d: %d\n",i,a+b);
+    }
+  return 0;
+}
