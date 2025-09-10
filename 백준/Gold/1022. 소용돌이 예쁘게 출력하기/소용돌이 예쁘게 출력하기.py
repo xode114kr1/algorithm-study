8b2f5fa060d5c57b1559de47b7a56ed886229470
xode114kr1 <144416796+xode114kr1@users.noreply.github.com>
2025-09-11 00:00:56 +0900

    [Gold III] Title: 소용돌이 예쁘게 출력하기, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/1022. 소용돌이 예쁘게 출력하기/소용돌이 예쁘게 출력하기.py
+++ 백준/Gold/1022. 소용돌이 예쁘게 출력하기/소용돌이 예쁘게 출력하기.py
@@ -3,15 +3,17 @@ def find_idx(r, c):
         return 1
     degree = max(abs(r), abs(c))
     st_idx = (degree - 1) * (16 + (degree - 2) * 8) // 2 + 2
-    st_r, st_c = degree - 1, degree
-    if r <= degree - 1 and c == degree:
+    if r <=  degree - 1 and c == degree:
         return st_idx + (degree - 1 - r)
+
     elif r == -1 * degree:
         st = st_idx + 2 * degree - 1
         return st + (degree - c)
+
     elif c == -1 * degree:
         st = st_idx + 4 * degree - 1
         return st + (degree + r)
+
     else:
         st = st_idx + 6 * degree - 1
         return st + (degree + c)
@@ -19,13 +21,12 @@ def find_idx(r, c):
 
 r1, c1, r2, c2 = map(int, input().split())
 
-max_val = 0
-for r in range(r1, r2 + 1):
-    for c in range(c1, c2 + 1):
-        max_val = max(max_val, find_idx(r, c))
-width = len(str(max_val))
+board = [
+    [find_idx(r, c) for c in range(c1, c2 + 1)]
+    for r in range(r1, r2 + 1)
+]
 
-for r in range(r1, r2 + 1):
-    for c in range(c1, c2 + 1):
-        print(str(find_idx(r, c)).rjust(width), end=" ")
-    print()
+width = len(str(max(map(max, board))))
+
+for row in board:
+    print(" ".join(str(v).rjust(width) for v in row))
