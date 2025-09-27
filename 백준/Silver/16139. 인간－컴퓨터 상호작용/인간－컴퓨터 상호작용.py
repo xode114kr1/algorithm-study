f627d0f3a940e52c10faba1617b81b85d66dc385
xode114kr1 <144416796+xode114kr1@users.noreply.github.com>
2025-09-27 14:47:41 +0900

    [Silver I] Title: 인간-컴퓨터 상호작용, Time: 300 ms, Memory: 153096 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.py
+++ 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.py
@@ -1,16 +1,19 @@
-s = input()
+import sys
+input = sys.stdin.readline
+
+s = input().strip()
 n = int(input())
 
 mat = [[0] * (len(s) + 1) for _ in range(26)]
 
 for i in range(1, len(s) + 1):
     ch_idx = ord(s[i - 1]) - ord('a')
+    for k in range(26):
+        mat[k][i] = mat[k][i - 1]
     mat[ch_idx][i] += 1
-    for j in range(i, len(s) + 1):
-        mat[ch_idx][j] = mat[ch_idx][i]
 
 for _ in range(n):
     c, st, en = input().split()
     st, en = int(st), int(en)
-    print(mat[ord(c) - ord('a')][en + 1] - mat[ord(c) - ord('a')][st])
-
+    idx = ord(c) - ord('a')
+    print(mat[idx][en + 1] - mat[idx][st])
