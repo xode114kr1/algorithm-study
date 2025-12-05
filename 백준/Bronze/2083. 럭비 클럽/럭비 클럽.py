bbe700f11cf88c21e6113a672dde346ab59461e3
xode114kr1 <144416796+xode114kr1@users.noreply.github.com>
2025-12-05 13:59:50 +0900

    [Bronze IV] Title: 럭비 클럽, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/2083. 럭비 클럽/럭비 클럽.py
@@ -0,0 +1,12 @@
+while True:
+    name, age, weight = input().split()
+    age = int(age)
+    weight = int(weight)
+
+    if name == '#':
+        break
+
+    if age > 17 or weight >= 80:
+        print(name, "Senior")
+    else:
+        print(name, "Junior")
