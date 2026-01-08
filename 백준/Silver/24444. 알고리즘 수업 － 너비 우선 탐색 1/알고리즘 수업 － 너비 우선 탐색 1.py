5dc194209dabf5e32ae27d55eabbaa228cbd3941
xode114kr1 <144416796+xode114kr1@users.noreply.github.com>
2026-01-08 21:17:26 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 1, Time: 448 ms, Memory: 139404 KB -BaekjoonHub

---
+++ 백준/Silver/24444. 알고리즘 수업 － 너비 우선 탐색 1/알고리즘 수업 － 너비 우선 탐색 1.py
@@ -0,0 +1,30 @@
+from collections import defaultdict, deque
+
+n, m, r = map(int, input().split())
+dic = defaultdict(list)
+isVisited = [False] * (n + 1)
+cnt = 1
+ans = [0] * (n + 1)
+for _ in range(m):
+    start, end = map(int, input().split())
+    dic[start].append(end)
+    dic[end].append(start)
+
+for i in range(1, n + 1):
+    dic[i].sort()
+
+q = deque()
+q.append(r)
+isVisited[r] = True
+while q:
+    cur = q.popleft()
+
+    ans[cur] = cnt
+
+    for node in dic[cur]:
+        if isVisited[node]: continue
+        q.append(node)
+        isVisited[node] = True
+    cnt += 1
+for node in ans[1:]:
+    print(node)
