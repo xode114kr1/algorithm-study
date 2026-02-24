d786db126af7607c8aa11db7d13f1dd1588fd3b5
xode114kr1 <144416796+xode114kr1@users.noreply.github.com>
2026-02-25 00:15:18 +0900

    [level 3] Title: 섬 연결하기, Time: 0.42 ms, Memory: 9.19 MB -BaekjoonHub

---
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.py
@@ -0,0 +1,36 @@
+from collections import defaultdict, deque
+
+def solution(n, costs):
+    def isConnect(a, b):
+        dic = defaultdict(list)
+        for s, e, c in lst:
+            dic[s].append(e)
+            dic[e].append(s)
+            
+        q = deque()
+        isVisited = [False] * n
+        isVisited[a] = True
+        q.append(a)
+        
+        while q:
+            cur = q.popleft()
+            nxts = dic[cur]
+            if cur == b:
+                return True
+            for nxt in nxts:
+                if not isVisited[nxt]:
+                    isVisited[nxt] = True
+                    q.append(nxt)
+        return False
+    
+    costs.sort(key = lambda x:x[2])
+    lst = []
+    for s, e, c in costs:
+        if not isConnect(s, e):
+            lst.append([s, e, c])
+    ans = 0
+    for _, _, c in lst:
+        ans += c
+    return ans
+    # lst = [[0, 1, 1], [1, 3, 1], [0, 2, 2]]
+    # print(isConnect(1, 2))
