c6549a623a982732b25cf30c784bd258c2672718
xode114kr1 <144416796+xode114kr1@users.noreply.github.com>
2026-03-07 21:45:59 +0900

    [Silver I] Title: 회전 초밥, Time: 784 ms, Memory: 36032 KB -BaekjoonHub

---
+++ 백준/Silver/2531. 회전 초밥/회전 초밥.py
@@ -0,0 +1,30 @@
+from collections import defaultdict
+
+n, d, k, c = map(int, input().split())
+lst = [int(input()) for _ in range(n)]
+lst = lst[:] + lst[:]
+dic = { x : 0 for x in range(1, d + 1)}
+s = set()
+
+for i in range(k):
+    dic[lst[i]] += 1
+    s.add(lst[i])
+
+dic[c] += 1
+s.add(c)
+
+ans = len(s)
+
+for i in range(n - 1):
+    left = i
+    lnum = lst[left]
+    dic[lnum] -= 1
+    if dic[lnum] == 0:
+        s.remove(lnum)
+
+    right = i + k
+    rnum = lst[right]
+    dic[rnum] += 1
+    s.add(rnum)
+    ans = max(ans, len(s))
+print(ans)
