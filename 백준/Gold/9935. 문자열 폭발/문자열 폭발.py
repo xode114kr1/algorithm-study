a0e362b57a950d26e72440a959079174daaa248a
xode114kr1 <144416796+xode114kr1@users.noreply.github.com>
2026-04-04 17:24:33 +0900

    [Gold IV] Title: 문자열 폭발, Time: 304 ms, Memory: 209416 KB -BaekjoonHub

--- 백준/Gold/9935. 문자열 폭발/문자열 폭발.py
+++ 백준/Gold/9935. 문자열 폭발/문자열 폭발.py
@@ -1,12 +1,11 @@
 s = input()
 boom = list(input())
 b_len = len(boom)
-last = boom[-1]
 
 ans = []
 for idx, c in enumerate(s):
     ans.append(c)
-    if c == last and ans[-b_len:] == boom:
+    while ans[-b_len:] == boom:
         del ans[-b_len:]
 if ans:
     print(''.join(ans))
