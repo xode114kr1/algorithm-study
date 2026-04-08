a0a6e68a052c047cf34819ef72c9c7b4eb53177d
xode114kr1 <144416796+xode114kr1@users.noreply.github.com>
2026-04-08 16:38:31 +0900

    [Gold V] Title: 합분해, Time: 104 ms, Memory: 112540 KB -BaekjoonHub

---
+++ 백준/Gold/2225. 합분해/합분해.py
@@ -0,0 +1,11 @@
+n, k = map(int, input().split())
+dp = [[0] * (n + 1) for _ in range(k + 1)] # i개 사용해서 k를 만들 수 있는 수
+for i in range(k + 1):
+    dp[i][0] = 1
+
+for i in range(1, k + 1):
+    for j in range(1, n + 1):
+        dp[i][j] = dp[i][j - 1] + dp[i - 1][j]
+
+print(dp[k][n] % 1000000000)
+
