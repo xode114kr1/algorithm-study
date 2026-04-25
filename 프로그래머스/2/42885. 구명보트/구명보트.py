ebb83122d215bf62d4b13fb2031e749ef48fbdb7
xode114kr1 <144416796+xode114kr1@users.noreply.github.com>
2026-04-25 19:38:37 +0900

    [level 2] Title: 구명보트, Time: 9.74 ms, Memory: 9.56 MB -BaekjoonHub

---
+++ 프로그래머스/2/42885. 구명보트/구명보트.py
@@ -0,0 +1,12 @@
+def solution(people, limit):
+    people.sort()
+    left, right = 0, len(people) - 1
+    cnt = 0
+    while left <= right:
+        if people[left] + people[right] <= limit:
+            left += 1
+            right -= 1
+        else:
+            right -= 1
+        cnt += 1
+    return cnt
